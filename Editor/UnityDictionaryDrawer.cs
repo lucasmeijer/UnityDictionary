71b5678e68e5584c997a918b7cc42c1256322a94
Richard Fine <rfine@tbrf.net>
2013-09-15 01:17:46 +0100

    start over with the PropertyDrawer - basic foldout for now

--- Editor/UnityDictionaryDrawer.cs
+++ Editor/UnityDictionaryDrawer.cs
@@ -5,25 +5,31 @@ public class UnityDictionaryDrawer<TKey, TValue> : PropertyDrawer
 {
     override public void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        EditorGUI.BeginProperty(position, label, property);
+        var propTitle = EditorGUI.BeginProperty(position, label, property);
 
-        var keysProperty = property.FindPropertyRelative("_keys");
-        var valuesProperty = property.FindPropertyRelative("_values");
+        // PropertyDrawers don't support GUILayout so we have to construct our own rectangles
+        var foldoutRect = new Rect(position.left, position.top, position.width, base.GetPropertyHeight(property, label));
+        bool wasExpanded = property.isExpanded;
+        property.isExpanded = EditorGUI.Foldout(foldoutRect, property.isExpanded, propTitle);
 
-        for(int i = 0; i < keysProperty.arraySize; ++i)
+        // Don't use the latest isExpanded value because if it's changed then we're the wrong height
+        if(wasExpanded)
         {
-            var keyProperty = keysProperty.GetArrayElementAtIndex(i);
-            var valueProperty = valuesProperty.GetArrayElementAtIndex(i);
-
-            var subRect = position;
-            subRect.xMax -= position.width/2;
-            EditorGUI.PropertyField(subRect, keyProperty);
-
-            subRect.x += position.width/2;
-            EditorGUI.PropertyField(subRect, valueProperty);
+            
         }
 
         EditorGUI.EndProperty();
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        var total = base.GetPropertyHeight(property, label);
+        if(property.isExpanded)
+        {
+            total += base.GetPropertyHeight(property, label) * property.FindPropertyRelative("_keys").arraySize;
+        }
+
+        return total;
     }
 }
 
